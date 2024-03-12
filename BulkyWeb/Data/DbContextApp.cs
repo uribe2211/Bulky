475bc55fefdd0d8e3e2b7fb64c60a81c2d7bcf51
Felipe Rodriguez <uribe2211@hotmail.com>
2024-03-12 00:01:44 -0600

    Category table

---
+++ BulkyWeb/Data/DbContextApp.cs
@@ -0,0 +1,13 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class DbContextApp : DbContext
+    {
+        public DbContextApp(DbContextOptions<DbContextApp>options):base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }=string.Empty;
+        public int DisplayOrder { get; set; }
+    }
+}
