3779ed719adf0d5ae8ae63f1b01e4eafc92ce52c
Felipe Rodriguez <uribe2211@hotmail.com>
2024-03-18 23:26:31 -0600

    Refactor project

---
+++ Bulky.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [StringLength(100)]
+    [MaxLength(100)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [Range(0, 100, ErrorMessage = "Only 0 to 100 values")]
+    [DisplayName("Display order")]
+    public int DisplayOrder { get; set; }
+}
